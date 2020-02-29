c9f7fdca3f6b06fb3d87e5693f2725a10fed58d4
Kathryn Wulf <kathrynjwulf@gmail.com>
2020-02-29 10:49:55 -0500

    //Final flex sensor code

--- test-flex-sensor.ino
+++ test-flex-sensor.ino
@@ -5,8 +5,8 @@ Joystick_ Joystick;
 void setup() 
 {
   Serial.begin(9600);
- // pinMode(A2, INPUT); //Button 3, ring finger
-  //pinMode(11, OUTPUT); //Button 3, ring finger
+  pinMode(A2, INPUT); //Button 3, ring finger
+  pinMode(11, OUTPUT); //Button 3, ring finger
   pinMode(A1, INPUT);//Button 2, middle finger
   pinMode(10, OUTPUT); //Button 2, middle finger
   pinMode(A0, INPUT);//Button 1, index finger
@@ -19,68 +19,43 @@ void loop()
   //INDEX FINGER
   int flex0 = analogRead(A0);
   Serial.println(flex0);
-  if(flex0 < 350) {
+  if(flex0 < 340) {
     Joystick.setButton(0, true);
     digitalWrite(9, HIGH);
-  } else if (flex0 > 450) {
+  } else if (flex0 > 410) {
     Joystick.setButton(0, false);
     digitalWrite(9, LOW);
-    //this is me placing the sensor on the glove and bending my finger, varies a little bit on placement so it's a very rough estimate
-  } // lowest value, finger bent, me: 450
+  }//760, 625
+  //flex0 < 350
+  //flex0 > 450
+  // aiden: 330, 430
   
   
 
 //MIDDLE FINGER
-
   int flex1 = analogRead(A1);
   Serial.println(flex1);
-  if(flex1 < 350) {
+  if(flex1 < 375) {
     Joystick.setButton(1, true);
     digitalWrite(10, HIGH);
-  } else if (flex1 > 450) {
+  } else if (flex1 > 410) {
     Joystick.setButton(1, false);
     digitalWrite(10, LOW);
-   }
+   }//690, 800
+   //aiden: 360, 450n
    
-  //lowest value, finger bent, me: 375
+   
    
    //RING FINGER
-   int flex2= analogRead(A2);
+  int flex2= analogRead(A2);
   Serial.println(flex2);
-  if(flex2 < 350) {
+  if(flex2 < 390) {
     Joystick.setButton(2, true);
     digitalWrite(11, HIGH);
-  } else if (flex2 > 450) {
+  } else if (flex2 > 420){
     Joystick.setButton(2, false);
     digitalWrite(11, LOW);
-  }// lowest value, finger bent, me: kinda janky, around 450
+  }//700, 800
+  //aiden: 370, 440
   delay(50);
 }
-
-/*
- * This is the arduino in the main code:
- * 
- * 
- * arduinoLeonardo = new Joystick(2);
- * 
- * if (gamepad.getName().equals("Arduino Leonardo")) {
- * int newglove = gamepad.getPort();
- * int newcon = arduinoLeonardo.getPort();
- * gamepad = new Joystick(newcon);
- * arduinoLeonardo = new Joystick(newglove);
- * }
- * 
- * if (extremeJoystick.getName().equals("Arduino Leonardo")){
- * int newjoy = arduinoLeonardo.getPort();
- * int newglove = extremeJoystick.getPort();
- * extremeJoystick = new Joystick(newjoy);
- * arduinoLeonardo = new Joystick(newglove);
- * }
- * //extremejoystick is logitech joystick extreme 3dpro
- * //gamepad = logitech controller (looks like xbox controller)
- * 
- * 
- * 
- * 
- * */
- */
